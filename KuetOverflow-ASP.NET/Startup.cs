ab6c983f0e7cec848c4ba36efe9d31d3f32e9415
Bikash <bikash11roy@gmail.com>
2017-02-21 11:57:05 +0600

    File serve connected

--- KuetOverflow-ASP.NET/Startup.cs
+++ KuetOverflow-ASP.NET/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -28,10 +23,7 @@ namespace KuetOverflow_ASP.NET
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseFileServer();
         }
     }
 }
