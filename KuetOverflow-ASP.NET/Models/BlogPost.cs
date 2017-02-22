62be8a2b88fabde246cc1719783c15d7a7c9084b
Bikash <bikash11roy@gmail.com>
2017-02-22 23:05:19 +0600

    BlogPost model created

---
+++ KuetOverflow-ASP.NET/Models/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KuetOverflow_ASP.NET.Models
+{
+    public class BlogPost
+    {
+        public long Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
