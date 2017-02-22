7ab5c55a26267ce1262c54da5cdaf03a1adb051f
Bikash <bikash11roy@gmail.com>
2017-02-22 22:27:33 +0600

    Question page Created

---
+++ KuetOverflow-ASP.NET/Controllers/QuestionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace KuetOverflow_ASP.NET.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
