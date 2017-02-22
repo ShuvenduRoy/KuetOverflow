298d32cc8ddd20cf7ac3393edf34cb9f65a584cf
Bikash <bikash11roy@gmail.com>
2017-02-22 22:43:02 +0600

    Blog Controller Created

---
+++ KuetOverflow-ASP.NET/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KuetOverflow_ASP.NET.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
