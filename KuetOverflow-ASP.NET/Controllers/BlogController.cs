527a64e6ec4e68d5954cd0953a7cb964240aeb4d
Bikash <bikash11roy@gmail.com>
2017-02-23 01:22:12 +0600

    Retriving data from database and showing with link

--- KuetOverflow-ASP.NET/Controllers/BlogController.cs
+++ KuetOverflow-ASP.NET/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using KuetOverflow_ASP.NET.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,49 +15,15 @@ namespace KuetOverflow_ASP.NET.Controllers
             _db = db;
         }
 
-        public IActionResult Post()
+        public IActionResult Post(long id)
         {
-            var post =
-                new BlogPost
-                {
-                    Title = "Test Ttiel",
-                    Author = "Shuvendu Roy",
-                    Description =
-                        "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
-                    DateTime = DateTime.Today
-                };
+            var post =_db.Posts.FirstOrDefault(x=>x.Id==id);
             return View(post);
         }
 
         public IActionResult Index()
         {
-            var posts = new[]
-            {
-                new BlogPost
-                {
-                    Title = "Test Ttiel",
-                    Author = "Shuvendu Roy",
-                    Description =
-                        "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
-                    DateTime = DateTime.Today
-                },
-                new BlogPost
-                {
-                    Title = "Test Ttiel",
-                    Author = "Shuvendu Roy",
-                    Description =
-                        "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
-                    DateTime = DateTime.Today
-                },
-                new BlogPost
-                {
-                    Title = "Test Ttiel",
-                    Author = "Shuvendu Roy",
-                    Description =
-                        "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.",
-                    DateTime = DateTime.Today
-                }
-            };
+            var posts = _db.Posts.ToArray();
             return View(posts);
         }
 
