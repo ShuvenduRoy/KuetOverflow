f3810992860ce6956e8c30427b6212b20ebefcf0
Bikash <bikash11roy@gmail.com>
2017-04-16 11:05:42 +0600

    About page craeted to show statictical summary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,37 @@
+using System.Linq;
+using System.Threading.Tasks;
+using KuetOverflow.Data;
+using KuetOverflow.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KuetOverflow.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in _context.Students
+                group student by student.EnrollmentDate
+                into dataGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dataGroup.Key,
+                    StudentCount = dataGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Error()
