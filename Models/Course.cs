9f10cf85bdfd0925823120328237cb02d397fdd3
Bikash <bikash11roy@gmail.com>
2017-04-16 19:38:16 +0600

    Course data annotation changed

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KuetOverflow.Models
@@ -6,10 +7,17 @@ namespace KuetOverflow.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0,5)]
         public int Credits { get; set; }
 
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
