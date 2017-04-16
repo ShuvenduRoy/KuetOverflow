de451696d6b1a0af1b3b6521fc5861dfa88b6b4a
Bikash <bikash11roy@gmail.com>
2017-04-16 11:10:17 +0600

    Initial migrations created

---
+++ Migrations/20170416050930_InitialCreate.Designer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using KuetOverflow.Data;
+
+namespace KuetOverflow.Migrations
+{
+    [DbContext(typeof(SchoolContext))]
+    [Migration("20170416050930_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("KuetOverflow.Models.Course", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("Credits");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("CourseID");
+
+                    b.ToTable("Course");
+                });
+
+            modelBuilder.Entity("KuetOverflow.Models.Enrollment", b =>
+                {
+                    b.Property<int>("EnrollmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CourseID");
+
+                    b.Property<int?>("Grade");
+
+                    b.Property<int>("StudentID");
+
+                    b.HasKey("EnrollmentID");
+
+                    b.HasIndex("CourseID");
+
+                    b.HasIndex("StudentID");
+
+                    b.ToTable("Enrollment");
+                });
+
+            modelBuilder.Entity("KuetOverflow.Models.Student", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("EnrollmentDate");
+
+                    b.Property<string>("FirstMidName");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Student");
+                });
+
+            modelBuilder.Entity("KuetOverflow.Models.Enrollment", b =>
+                {
+                    b.HasOne("KuetOverflow.Models.Course", "Course")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("CourseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("KuetOverflow.Models.Student", "Student")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("StudentID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
