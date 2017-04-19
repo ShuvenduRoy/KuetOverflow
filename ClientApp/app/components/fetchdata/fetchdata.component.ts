1db8cb9ae26cd03bcdc30e3bc833939db313eceb
Bikash <bikash11roy@gmail.com>
2017-04-19 17:46:13 +0600

    Footer created and admin page linked

--- ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
-    selector: 'fetchdata',
+    selector: 'footer',
     templateUrl: './fetchdata.component.html'
 })
 export class FetchDataComponent {
