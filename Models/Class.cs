a959a8c80f466f991dc739ffb940340c8aa155ad
Bikash <bikash11roy@gmail.com>
2017-05-02 03:00:49 +0600

    Class model created

---
+++ Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace KuetOverflow.Models
+{
+    public class Class
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
