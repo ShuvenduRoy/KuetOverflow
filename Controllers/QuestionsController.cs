7d6aa8df05e7dc764231891bee66a73c52ee7cd9
Bikash <bikash11roy@gmail.com>
2017-05-01 13:32:47 +0600

    Question details presentations changed

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using KuetOverflow.Data;
 using KuetOverflow.Models;
 using KuetOverflow.Models.SchoolViewModels;
-using Microsoft.AspNetCore.Identity;
 
 namespace KuetOverflow.Controllers
 {
