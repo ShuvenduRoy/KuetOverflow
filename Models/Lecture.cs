1ebf18af7ce060b168e81fa670e835d8435370a1
Bikash <bikash11roy@gmail.com>
2017-05-02 03:27:38 +0600

    Course referance added to Lecture Model

--- Models/Lecture.cs
+++ Models/Lecture.cs
@@ -5,6 +5,7 @@ namespace KuetOverflow.Models
     public class Lecture
     {
         public int ID { get; set; }
+        public int CourseId { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public DateTime UpdateTime { get; set; }
