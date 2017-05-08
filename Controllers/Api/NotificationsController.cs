efd6e629fbcef4d8a1af01f07c03ef1ae547c2c9
Bikash <bikash11roy@gmail.com>
2017-05-08 08:40:43 +0600

    Notification returen via api

---
+++ Controllers/Api/NotificationsController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using KuetOverflow.Data;
+using KuetOverflow.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KuetOverflow.Controllers.Api
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/Notifications")]
+    public class NotificationsController : Controller
+    {
+        private SchoolContext _context;
+
+        public NotificationsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Notification> GetNotifications()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var notifications = _context.UserNotifications
+                .Where(n => n.UserId == userId)
+                .Select(un => un.Notification)
+                .ToList();
+
+            return notifications;
+
+        }
+    }
+}
