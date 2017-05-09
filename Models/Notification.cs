d7910564e049898ad4dbeb25d1dbab16590e117c
Bikash <bikash11roy@gmail.com>
2017-05-09 20:29:05 +0600

    Date bug fixed

--- Models/Notification.cs
+++ Models/Notification.cs
@@ -7,7 +7,6 @@ namespace KuetOverflow.Models
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Body { get; set; }
-        public DateTime DateTime { get; set; }
         public DateTime Time { get; set; }
     }
 }
