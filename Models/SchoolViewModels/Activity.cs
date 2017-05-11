eba67a8e829ffd6d956aa2dc2938fc2ef60a7643
Bikash <bikash11roy@gmail.com>
2017-05-11 08:15:21 +0600

    Navbar changed to have account and activity

---
+++ Models/SchoolViewModels/Activity.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace KuetOverflow.Models.SchoolViewModels
+{
+    public class Activity
+    {
+        public IEnumerable<Question> Questions { get; set; }
+        public IEnumerable<Answer> Answers { get; set; }
+    }
+}
