18614656acb8c7807f90390c6e5d70e768bc572a
Bikash <bikash11roy@gmail.com>
2017-05-11 08:54:43 +0600

    Tweet model created

---
+++ Models/Tweet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KuetOverflow.Models
+{
+    public class Tweet
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public string UserId { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
