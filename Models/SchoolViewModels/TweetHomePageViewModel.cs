0a87cca56153102499475c9483d5fa1f6e98611a
Bikash <bikash11roy@gmail.com>
2017-05-13 09:50:13 +0600

    Tweet Home page view model

---
+++ Models/SchoolViewModels/TweetHomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace KuetOverflow.Models.SchoolViewModels
+{
+    public class TweetHomePageViewModel
+    {
+        public TwitterUser User { get; set; }
+        public IEnumerable<Tweet> Tweets { get; set; }
+    }
+}
