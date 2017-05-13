75de8dc68f218aad2236f8a26bddc70a7157df8e
Bikash <bikash11roy@gmail.com>
2017-05-14 02:18:52 +0600

    Follow Api Created

---
+++ Controllers/Api/FollowController.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using System.Security.Claims;
+using KuetOverflow.Data;
+using KuetOverflow.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KuetOverflow.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Follow")]
+    public class FollowController : Controller
+    {
+
+        private SchoolContext _context;
+        private UserManager<ApplicationUser> _userManager;
+
+        public FollowController(SchoolContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public void AddFollower(int id)
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _context.TwitterUsers
+                .SingleOrDefault(u => u.UserID == userId);
+
+            Follow follow = new Follow
+            {
+                FollowerId = id,
+                FolloweeId = user.ID
+            };
+
+            _context.Follows.AddAsync(follow);
+            _context.SaveChangesAsync();
+        }
+    }
+}
