2304f54ae37b83cfe4c3a3dcedf2e61e9f062208
Bikash <bikash11roy@gmail.com>
2017-05-14 01:59:59 +0600

    Follow model created

---
+++ Models/Follow.cs
@@ -0,0 +1,12 @@
+﻿namespace KuetOverflow.Models
+{
+    public class Follow
+    {
+        public int ID { get; set; }
+        public int FollowerId { get; set; }
+        public int FolloweeId { get; set; }
+
+        public TwitterUser Follower { get; set; }
+        public TwitterUser Followee { get; set; }
+    }
+}
