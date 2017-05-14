22aa7444d4cab3fc1999f8145a3a81755e8d8566
Bikash <bikash11roy@gmail.com>
2017-05-15 00:11:56 +0600

    databse updated

--- Migrations/20170514181119_TweetModeladdedUserId.Designer.cs
+++ Migrations/20170514181119_TweetModeladdedUserId.Designer.cs
@@ -8,9 +8,10 @@ using KuetOverflow.Data;
 namespace KuetOverflow.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170514181119_TweetModeladdedUserId")]
+    partial class TweetModeladdedUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
---
+++ Migrations/20170514181119_TweetModeladdedUserId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KuetOverflow.Migrations
+{
+    public partial class TweetModeladdedUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TweetUserID",
+                table: "Tweet",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TweetUserID",
+                table: "Tweet");
+        }
+    }
+}
