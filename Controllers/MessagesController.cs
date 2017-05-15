225370d5118fe6d19569dfddcb02344dbfbe1bb0
Bikash <bikash11roy@gmail.com>
2017-05-16 02:42:09 +0600

    User Message action added

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -25,6 +25,11 @@ namespace KuetOverflow.Controllers
             return View(await _context.Messages.ToListAsync());
         }
 
+        public async Task<IActionResult> UserMessages()
+        {
+            return View(await _context.Messages.ToListAsync());
+        }
+
         // GET: Messages/Details/5
         public async Task<IActionResult> Details(int? id)
         {
