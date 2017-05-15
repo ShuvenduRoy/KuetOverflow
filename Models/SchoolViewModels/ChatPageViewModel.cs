86e81f33b47611185981ca1fa6ca3a97223b5a06
Bikash <bikash11roy@gmail.com>
2017-05-16 02:53:53 +0600

    ChatPage View model added

---
+++ Models/SchoolViewModels/ChatPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KuetOverflow.Models.SchoolViewModels
+{
+    public class ChatPageViewModel
+    {
+        public IEnumerable<Message> Messages { get; set; }
+        public IEnumerable<TwitterUser> Users { get; set; }
+    }
+}
