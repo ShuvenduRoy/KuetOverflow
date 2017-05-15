bd7b5794471a697abe721f233a33939d50a00367
Bikash <bikash11roy@gmail.com>
2017-05-16 02:33:38 +0600

    Message Model added

---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KuetOverflow.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool IsSeen { get; set; } = false;
+    }
+}
